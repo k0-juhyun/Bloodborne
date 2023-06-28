0576ee2ca344d770ea186b667fffbf5ef94a8228
metagitthe <metav3010@gmail.com>
2023-06-29 08:44:34 +0900

    0628

---
+++ Assets/YJ/Scripts/Boss.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    // 현재 상태
+    int state;          // 0: 대기, 1:걷기, 2:대시, 3:점프 공격, 4: 회피, 5: 피격, 6:낫 공격, 7: 칼 공격, 8: 총 공격
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/YJ/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 이동 속도
+    [Header("이동 속도")]
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
