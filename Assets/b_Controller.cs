a0c25bc0e5147f741446d0e81e083a878800b533
k0-juhyun <79074052+k0-juhyun@users.noreply.github.com>
2023-06-29 14:40:23 +0900

    3
    3

--- Assets/b_Controller.cs
+++ Assets/b_Controller.cs
@@ -8,23 +8,42 @@ public class b_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        print(collision.gameObject.name);
+        if(collision.gameObject.CompareTag("p_Weapon"))
+        {
+
+        }
     }
 
     protected virtual void WhichDirectionDamageCameFrom(float direction)
     {
-        if (direction >= 145 && direction <= 180)
+        //forward
+        if (direction >= 225 && direction <= 315)
+        {
+
+        }
+        //right
+        else if (direction >= 315 && direction <= 45)
+        {
+
+        }
+        //back
+        else if(direction >= 45 && direction <= 135)
+        {
+
+        }
+        //left
+        else if(direction >= 135 && direction <= 225)
         {
 
         }
