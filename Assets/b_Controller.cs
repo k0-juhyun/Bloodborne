81fa62d4fe4d2471bc9026e9c1c226eaa142344c
k0-juhyun <79074052+k0-juhyun@users.noreply.github.com>
2023-06-29 18:16:59 +0900

    11

--- Assets/b_Controller.cs
+++ Assets/b_Controller.cs
@@ -2,73 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class b_Controller : MonoBehaviour
-{
-    [Header("플레이어")]
-    public GameObject Player;
-
-    string currentDamageAnimation;
-
-    // 컴포넌트들
-    Animator animator;
-    void Start()
+    public class b_Controller : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-    }
+        [Header("플레이어")]
+        public GameObject Player;
 
-    void Update()
-    {
-        
+        string currentDamageAnimation;
 
-    }
-
-
-    float GetAngle(Vector3 from, Vector3 to)
-    {
-        return Quaternion.FromToRotation(transform.forward, to - from).eulerAngles.y;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("p_Weapon"))
+        // 컴포넌트들
+        Animator animator;
+        void Start()
         {
-            float directionHitFrom = (GetAngle(transform.position, Player.transform.position));
-            WhichDirectionDamageCameFrom(directionHitFrom);
-            animator.Play(currentDamageAnimation);
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            WhichPositionLookAt(Player.transform.position);
+        }
+
+
+        float GetAngle(Vector3 from, Vector3 to)
+        {
+            return Quaternion.FromToRotation(transform.forward, to - from).eulerAngles.y;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("p_Weapon"))
+            {
+                float directionHitFrom = (GetAngle(transform.position, Player.transform.position));
+                WhichDirectionDamageCameFrom(directionHitFrom);
+                animator.Play(currentDamageAnimation);
+            }
+        }
+
+        protected virtual void WhichDirectionDamageCameFrom(float direction)
+        {
+            //forward
+            if (direction >= 0 && direction < 45)
+            {
+                currentDamageAnimation = "GetHitFront";
+                print("Forward");
+            }
+            if (direction >= 315 && direction < 360)
+            {
+                currentDamageAnimation = "GetHitFront";
+                print("Forward");
+            }
+            //Right
+            else if (direction >= 45 && direction < 135)
+            {
+                currentDamageAnimation = "GetHitRight";
+                print("Right");
+            }
+            //Back
+            else if (direction >= 135 && direction < 225)
+            {
+                currentDamageAnimation = "GetHitBack";
+                print("Back");
+            }
+            //Left
+            else if (direction >= 225 && direction <= 315)
+            {
+                currentDamageAnimation = "GetHitLeft";
+                print("Left");
+            }
+            return;
+        }
+
+        Vector3 WhichPositionLookAt(Vector3 playerPos)
+        {
+            float lookDirection = GetAngle(transform.position, Player.transform.position);
+            playerPos = Player.transform.position.normalized;
+            if (lookDirection >= 0 && lookDirection < 45)
+            {
+                gameObject.transform.LookAt(playerPos);
+                print("look");
+            }
+            else if (lookDirection >= 315 && lookDirection < 360)
+            {
+                gameObject.transform.LookAt(playerPos);
+                print("look");
+            }
+            return playerPos;
         }
     }
-
-    protected virtual void WhichDirectionDamageCameFrom(float direction)
-    {
-        //forward
-        if (direction >= 0 && direction < 45)
-        {
-            currentDamageAnimation = "GetHitFront";
-            print("Forward");
-        }
-        if(direction >= 315 && direction < 360)
-        {
-            currentDamageAnimation = "GetHitFront";
-            print("Forward");
-        }
-        //Right
-        else if (direction >= 45 && direction < 135)
-        {
-            currentDamageAnimation = "GetHitRight";
-            print("Right");
-        }
-        //Back
-        else if (direction >= 135 && direction < 225)
-        {
-            currentDamageAnimation = "GetHitBack";
-            print("Back");
-        }
-        //Left
-        else if (direction >= 225 && direction <= 315)
-        {
-            currentDamageAnimation = "GetHitLeft";
-            print("Left");
-        }
-        return;
-    }
-}
--- Assets/c_Rotate.cs
+++ Assets/c_Rotate.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class c_Rotate : MonoBehaviour
+namespace Retro.ThirdPersonCharacter
 {
-    float rx;
-    float ry;
-    public float rotSpeed = 300;
-    // Start is called before the first frame update
-    void Start()
+    public class c_Rotate : MonoBehaviour
     {
+        float rx;
+        float ry;
+        public float rotSpeed = 300;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // 1. 사용자의 입력에따라
-        float mx = Input.GetAxis("Mouse X");
-        float my = Input.GetAxis("Mouse Y");
-        // 2. X와 Y축의 값을 누적하고
-        rx += my * rotSpeed * Time.deltaTime;
-        ry += mx * rotSpeed * Time.deltaTime;
-        // 3. rx에대해 각도를 제한하고싶다.
-        rx = Mathf.Clamp(rx, -50, 75);
-        ry = Mathf.Clamp(ry, -180, 180);
-        // 4. 그 누적값으로 회전을 하고싶다.
-        transform.eulerAngles = new Vector3(-rx, ry, 0);
+        // Update is called once per frame
+        void Update()
+        {
+            // 1. 사용자의 입력에따라
+            float mx = Input.GetAxis("Mouse X");
+            float my = Input.GetAxis("Mouse Y");
+            // 2. X와 Y축의 값을 누적하고
+            rx += my * rotSpeed * Time.deltaTime;
+            ry += mx * rotSpeed * Time.deltaTime;
+            // 3. rx에대해 각도를 제한하고싶다.
+            rx = Mathf.Clamp(rx, -50, 75);
+            ry = Mathf.Clamp(ry, -180, 180);
+            // 4. 그 누적값으로 회전을 하고싶다.
+            transform.eulerAngles = new Vector3(-rx, ry, 0);
+        }
     }
 }
