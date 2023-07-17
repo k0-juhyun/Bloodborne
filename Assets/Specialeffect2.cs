9def56611056d44a2117bbee56b56dde620fde95
k0-juhyun <79074052+k0-juhyun@users.noreply.github.com>
2023-07-17 20:37:24 +0900

    제발
    제발

--- Assets/Specialeffect2.cs
+++ Assets/Specialeffect2.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class Specialeffect2 : MonoBehaviour
 {
-    public Transform eyePosition;
-    private RectTransform canvasRect; 
+    public Transform bossEye; // 보스 눈 위치
+    private RectTransform canvasRect; // 캔버스 RectTransform
+    private RectTransform particleRect; // 파티클 시스템의 RectTransform
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         canvasRect = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+        particleRect = GetComponent<RectTransform>();
     }
 
-    void Update()
+    private void Update()
     {
-        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, eyePosition.position);
+        // 보스 눈 위치를 월드 좌표에서 캔버스 상의 위치로 변환
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, bossEye.position);
         Vector2 anchoredPos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, screenPos, null, out anchoredPos);
 
-        transform.localPosition = anchoredPos;
+        // 파티클 시스템의 위치를 부드럽게 업데이트
+        particleRect.anchoredPosition = Vector2.Lerp(particleRect.anchoredPosition, anchoredPos, Time.deltaTime * 5f);
     }
 }
