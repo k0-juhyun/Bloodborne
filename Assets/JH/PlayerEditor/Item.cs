5a63a108c5cca13795d2337f18192de1fb59cffd
k0-juhyun <imkojuhyun@gmail.com>
2023-07-23 01:00:54 +0900

    Weapon
    Weapon

---
+++ Assets/JH/PlayerEditor/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace bloodborne
+{
+    public class Item : ScriptableObject
+    {
+        [Header("Item Information")]
+        public string itemName;
+    }
+}
