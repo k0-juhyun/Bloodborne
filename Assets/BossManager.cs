3b939e0f8af33c2a0b01640d03342fccd20e61ba
k0-juhyun <imkojuhyun@gmail.com>
2023-07-24 20:57:31 +0900

    이상해..
    이상해..

--- Assets/BossManager.cs
+++ Assets/BossManager.cs
@@ -6,39 +6,6 @@ namespace bloodborne
 {
     public class BossManager : CharacterManager
     {
-        BossLocomotionManager bossLocomotionManager;
-        public bool isPerformingAction;
-        
-        [Header("A.I Settings")]
-        public float detectionRadius;
-        public float maximumDetectionAngle = 50;
-        public float minimumDetectionAngle = -50;
-
-        private void Start()
-        {
-            bossLocomotionManager= GetComponent<BossLocomotionManager>();
-        }
-
-        private void Update()
-        {
-            
-        }
-
-        public void HandleCurrentAction()
-        {
-            if(bossLocomotionManager.currentTarget == null)
-            {
-                bossLocomotionManager.HandleDetection();
-            }
-            else
-            {
-                bossLocomotionManager.HandleMoveToTarget();
-            }
-        }
-
-        private void FixedUpdate()
-        {
-            HandleCurrentAction();
-        }
+  
     }
 }
